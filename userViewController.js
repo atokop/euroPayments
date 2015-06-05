a4a753402b6b19d3752e015bfa0311f297033f5e
kwamina <atokop@gmail.com>
2015-06-04 22:32:09 -0400

    added in dummy polymer element

--- userViewController.js
+++ userViewController.js
@@ -1,12 +1,11 @@
-var myAppModule = angular.module('myApp', []);
-myAppModule.controller('userSelectController', function($scope, $http) {
-  $scope.users = [{uid:0, name: "me", photo:"res/kwamina.png"}, {uid:1, name: "you"}]
+var myAppModule2 = angular.module('myApp');
+myAppModule2.controller('userViewController', function($scope, $http) {
+  $scope.userss = [{uid:0, name: "me", photo:"res/kwamina.png"}, {uid:1, name: "you"}, {uid:2, name: "them"}, {uid:2, name: "their"}]
   $scope.usersLoaded=false;
 
   //TODO: Replace with endpoint to load user data
   $http.get('file:////Users/kwamina/dev/euro_payments/poly/index.html').success(function(data) {
     $scope.usersLoaded = true;
-    console.log($scope.users);
   }).
   error(function(data) {
     alert("unable to access backend");
